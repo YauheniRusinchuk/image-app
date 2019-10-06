7008d92bda276c0a802f14be27dea1fecfafa612
YauheniRusinchuk <ruevgal@gmail.com>
2019-10-06 16:35:52 +0300

    init and add sass

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Main() {
+    return (
+        <div className='main_container'>
+            <div className='main_wrapper'>
+
+            </div>
+        </div>
+    )
+}
+
+
+export default Main;
